f49c374f7cff3891c94e24cd6167a97214b5f99b
spencer <spencer8833@gmail.com>
2014-12-21 11:10:14 +0800

    Server connect
    confirm!

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
+<!DOCTYPE HTML>
+<html>	
+<head>
+	<Title>practice</Title>
+</head>
+
+<body>
+<form action=. method=POST>
+	ID:<input type=text name=id><br/>
+	Password:<input type=password name=pwd><br/>
+	<input type=hidden name=phptype value=1>
+	<input type=submit value=submit>
+</form>
 <?php
-	echo "<form action=. method=POST>";
-	echo "ID:<input type=text name=id><br/>";
-	echo "Password:<input type=password name=pwd><br/>";
-	echo "<input type=submit value=submit>";
-	echo "</form>";
+	$phptype = $_POST["phptype"];
 	$id = $_POST["id"];
-	$pwd = $_POST["pwd"];
-	mysql_connect("localhost","root","D10116232");
-	mysql_select_db("sun_member");
-	echo $id;
-	echo $pwd;
-	$str = "select * from sun_member";
-	$result = mysql_query($str);
-	echo count(mysql_fetch_array($result));
+	$pwd = $_POST["pwd"];	
+	if($phptype == 1){
+		mysql_connect("localhost","root","D10116232");
+		mysql_select_db("D10116232sql");
+		$str = "select * from sun_member where email='$id' and password='$pwd'";
+		$result = mysql_query($str);
+		if (mysql_num_rows($result) == 1)
+			echo "登入成功!";
+			else
+			echo "帳號或密碼有誤!";
+	}	
 ?>
+<body>
+
+</html>
